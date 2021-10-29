62f77e11cb70f8af462267cfbf3a33fea00b1872
Trent Henderson <trent.henderson1@outlook.com>
2021-10-29 15:01:30 +1100

    Some initial skeleton

---
+++ R/tidy_arff_files.R
@@ -0,0 +1,28 @@
+#------------------------------------------
+# This script sets out to load all the 
+# packages and folders necessary for the 
+# project
+#
+# NOTE: This script requires setup.R to
+# have been run first
+#-----------------------------------------
+
+#-----------------------------------------
+# Author: Trent Henderson, 29 October 2021
+#-----------------------------------------
+
+#' Function to load and process files into a tidy dataframe
+#' 
+#' @param filepath string specifying filepath to the classification files
+#' @return an object of class dataframe
+#' @author Trent Henderson
+#' 
+
+tidy_arff_files <- function(filepath){
+  
+  xx
+  
+  #------------ Save output ------------
+  
+  save(TimeSeriesData, file = "data/TimeSeriesData.Rda")
+}
