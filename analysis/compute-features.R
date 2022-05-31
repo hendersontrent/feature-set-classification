82ed40a46dfd5212897ea5d36dbd4101de7f0fe4
Trent Henderson <trent.henderson1@outlook.com>
2022-05-31 18:41:34 +1000

    Small syntax edits

--- analysis/compute-features.R
+++ analysis/compute-features.R
@@ -63,11 +63,12 @@ extract_features_by_problem <- function(data, theproblem, z_score = TRUE){
   
   if(tmp2$values[1] != tmp$values[1]){
     print(paste0("z-score applied to problem: ", theproblem))
+    z_tracker <<- append(z_tracker, theproblem) # Keep track of which problems were not z-scored
   }
   
   # Calculate features
   
-  outs <- calculate_features(tmp, id_var = "id", time_var = "timepoint", 
+  outs <- calculate_features(tmp2, id_var = "id", time_var = "timepoint", 
                              values_var = "values", group_var = "target", 
                              feature_set = c("catch22", "feasts", "tsfeatures", "tsfresh", "tsfel", "kats"), 
                              catch24 = TRUE, tsfresh_cleanup = FALSE, seed = 123)
@@ -78,6 +79,7 @@ extract_features_by_problem <- function(data, theproblem, z_score = TRUE){
 # Run the function
 
 extract_features_by_problem_safe <- purrr::possibly(extract_features_by_problem, otherwise = NULL)
+z_tracker <- c()
 
 unique(TimeSeriesData$problem) %>%
   purrr::map(~ extract_features_by_problem_safe(data = TimeSeriesData, theproblem = .x))
