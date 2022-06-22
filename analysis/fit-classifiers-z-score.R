fdd1edac1c95ff5007ee46d78e498ae1058686c4
Trent Henderson <trent.henderson1@outlook.com>
2022-06-22 18:26:29 +1000

    z-score version fixes

--- analysis/fit-classifiers-z-score.R
+++ analysis/fit-classifiers-z-score.R
@@ -29,34 +29,25 @@ rm(TimeSeriesData) # Clean up environment as dataframe is large
 #' @param theproblem filepath to the feature data
 #' @param tt_labels the dataframe containing train-test labels
 #' @param set Boolean whether to fit by set or not
-#' @param remove_catch24 Boolean whether to remove mean and SD from catch22 feature set
 #' @returns an object of class dataframe
 #' @author Trent Henderson
 #' 
 
-calculate_accuracy_by_problem <- function(theproblem, tt_labels, set = TRUE, remove_catch24 = TRUE){
+calculate_accuracy_by_problem <- function(theproblem, tt_labels, set = TRUE){
   
-  files <- list.files("data/feature-calcs", full.names = TRUE, pattern = "\\.Rda")
+  files <- list.files("data/feature-calcs/z-scored", full.names = TRUE, pattern = "\\.Rda")
   message(paste0("Doing problem ", match(theproblem, files), "/", length(files)))
-  
   load(theproblem)
-  
-  # Remove Mean and SD from catch22 if specified (e.g., for un-normalised data)
-  
-  if(remove_catch24){
-    outs <- outs %>%
-      filter(names %ni% c("DN_Mean", "DN_Spread_Std"))
-  }
   
   # Join in train-test indicator
   
-  outs <- outs %>%
+  outs_z <- outs_z %>%
     inner_join(tt_labels, by = c("id" = "id")) %>%
     dplyr::select(-c(problem))
   
   # Fit multi-feature classifiers by feature set
   
-  results <- fit_multi_feature_classifier_tt(outs, 
+  results <- fit_multi_feature_classifier_tt(outs_z, 
                                              id_var = "id", 
                                              group_var = "group",
                                              by_set = set, 
@@ -74,12 +65,12 @@ calculate_accuracy_by_problem_safe <- purrr::possibly(calculate_accuracy_by_prob
 data_files <- list.files("data/feature-calcs/z-scored", full.names = TRUE, pattern = "\\.Rda")
 
 outputs_z <- data_files %>%
-  purrr::map_df(~ calculate_accuracy_by_problem_safe(theproblem = .x, tt_labels = train_test_ids, set = TRUE, remove_catch24 = TRUE))
+  purrr::map_df(~ calculate_accuracy_by_problem_safe(theproblem = .x, tt_labels = train_test_ids, set = TRUE))
 
 # Run function using all features at once to form an aggregate comparison later
 
 outputs_aggregate_z <- data_files %>%
-  purrr::map_df(~ calculate_accuracy_by_problem_safe(theproblem = .x, tt_labels = train_test_ids, set = FALSE, remove_catch24 = TRUE))
+  purrr::map_df(~ calculate_accuracy_by_problem_safe(theproblem = .x, tt_labels = train_test_ids, set = FALSE))
 
 save(outputs_z, file = "data/outputs_z.Rda")
 save(outputs_aggregate_z, file = "data/outputs_aggregate_z.Rda") 
