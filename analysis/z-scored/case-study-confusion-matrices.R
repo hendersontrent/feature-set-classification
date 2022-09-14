d0c34578f3f51a2f02b5a35e07f75e8c7d0d7d6b
Trent Henderson <trent.henderson1@outlook.com>
2022-09-14 16:37:11 +1000

    case study cor plot

--- analysis/z-scored/case-study-confusion-matrices.R
+++ analysis/z-scored/case-study-confusion-matrices.R
@@ -176,26 +176,71 @@ View(plane_bin_top_2$ResultsTable)
 # MiddlePhalanxTW
 #----------------
 
+# Get data for tsfresh
+
 load("data/feature-calcs/z-scored/MiddlePhalanxTW.Rda")
-MiddlePhalanxTW <- outs_z
+MiddlePhalanxTW <- outs_z %>% filter(method == "tsfresh")
 rm(outs_z)
 
-# Define useful function for drawing plot between all feature values for a given set
+# Draw plot
 
-draw_cor_plots <- function(data, set){
+plot_feature_cors <- function(data, cor_method, clust_method){
   
-  tmp <- data %>%
-    filter(method == set)
+  # Wrangle dataframe
   
-  p <- plot_feature_correlations(data = tmp, id_var = "id", names_var = "names", 
-                                 values_var = "values", method = "z-score", 
-                                 clust_method = "average", interactive = FALSE)
+  cor_dat <- data %>%
+    dplyr::select(c(.data$id, .data$names, .data$values)) %>%
+    tidyr::drop_na() %>%
+    tidyr::pivot_wider(id_cols = .data$id, names_from = .data$names, values_from = .data$values) %>%
+    dplyr::select(-c(.data$id))
   
-  return(p)
+  # Delete features that contain NAs/Infs and features with constant values
+  # Note: We want to delete features rather than observations so the data stays consistent across sets
+  
+  cor_dat <- cor_dat %>%
+    dplyr::select(mywhere(~dplyr::n_distinct(.) > 1))
+  
+  inds <- apply(cor_dat, 2, function(x)!any(is.na(x)))
+  cor_dat <- cor_dat[, inds]
+  inds2 <- colSums(cor_dat[1:ncol(cor_dat)])
+  inds2 <- inds2[inds2 %ni% c(NA, Inf, -Inf, NaN)]
+  cor_dat <- cor_dat[, names(inds2)]
+  
+  # Calculate correlations and take absolute
+  
+  result <- abs(stats::cor(cor_dat, method = cor_method))
+  
+  # Perform clustering
+  
+  row.order <- stats::hclust(stats::dist(result, method = "euclidean"), method = clust_method)$order # Hierarchical cluster on rows
+  col.order <- stats::hclust(stats::dist(t(result), method = "euclidean"), method = clust_method)$order # Hierarchical cluster on columns
+  dat_new <- result[row.order, col.order] # Re-order matrix by cluster outputs
+  cluster_out <- reshape2::melt(as.matrix(dat_new)) # Turn into dataframe
+  
+  # Define a nice colour palette consistent with RColorBrewer in other functions
+  
+  mypalette <- c("#B2182B", "#D6604D", "#F4A582", "#FDDBC7", "#D1E5F0", "#92C5DE", "#4393C3", "#2166AC")
+  
+  # Draw plot
+  
+  FeatureFeatureCorrelationPlot <- cluster_out %>%
+    ggplot2::ggplot(ggplot2::aes(x = .data$Var1, y = .data$Var2)) +
+    ggplot2::geom_raster(ggplot2::aes(fill = .data$value)) +
+    ggplot2::labs(title = "Pairwise correlation matrix of tsfresh features for MiddlePhalanxTW",
+                  x = NULL,
+                  y = NULL,
+                  fill = "Absolute correlation coefficient") +
+    ggplot2::theme_bw() +
+    ggplot2::scale_fill_gradient2(low = "#0571B0",
+                                  mid = "white",
+                                  high = "#CA0020",
+                                  midpoint = 0.5) +
+    ggplot2::theme(panel.grid = ggplot2::element_blank(),
+                   legend.position = "bottom",
+                   axis.text = ggplot2::element_blank())
+  
+  return(FeatureFeatureCorrelationPlot)
 }
 
-MiddlePhalanxTW_plots <- c("tsfresh", "catch22") %>%
-  purrr::map(~ draw_cor_plots(data = MiddlePhalanxTW, set = .x))
-
-print(MiddlePhalanxTW_plots[[1]])
-print(MiddlePhalanxTW_plots[[2]])
+tsfresh_MiddlePhalanxTW <- plot_feature_cors(data = MiddlePhalanxTW, cor_method = "pearson", clust_method = "average")
+print(tsfresh_MiddlePhalanxTW)
