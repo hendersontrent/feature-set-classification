62982276db1b1ef070f14feee012e4c19f2e82f0
Trent Henderson <trent.henderson1@outlook.com>
2022-10-01 16:10:29 +1000

    plot works

--- analysis/non-z-scored/comp-to-benchmarks.R
+++ analysis/non-z-scored/comp-to-benchmarks.R
@@ -71,7 +71,8 @@ find_winners <- function(outputs_data, outputs_agg_data, benchmark_data){
   combns <- combns[combns$set1 != combns$set2, ]
   
   comps <- 1:nrow(combns) %>%
-    purrr::map_df(~ calculate_p_values_acc(data = all_data, combn_data = combns, rownum = .x))
+    purrr::map_df(~ calculate_p_values_acc(data = all_data, combn_data = combns, rownum = .x)) %>%
+    mutate(p_value_adj = p.adjust(p_value, method = "holm"))
   
   # Add in accuracy values to get direction
   
@@ -109,7 +110,7 @@ find_winners <- function(outputs_data, outputs_agg_data, benchmark_data){
       set2 %in% sets         ~ accuracy_set.y,
       set2 %in% benches      ~ accuracy_bench.y,
       set2 == "All features" ~ accuracy_all.y)) %>%
-    dplyr::select(c(problem, set1, set2, set1_accuracy, set2_accuracy, t_statistic, p_value))
+    dplyr::select(c(problem, set1, set2, set1_accuracy, set2_accuracy, t_statistic, p_value, p_value_adj))
 
   # Find best "features" for each problem
   
@@ -179,16 +180,17 @@ find_winners <- function(outputs_data, outputs_agg_data, benchmark_data){
       is.na(p_value)                                  ~ "Zero variance for one/more sets",
       p_value > .05                                   ~ "Non-Significant difference",
       p_value < .05 & set1_accuracy > set2_accuracy   ~ set1,
-      p_value < .05 & set1_accuracy < set2_accuracy   ~ set2))
+      p_value < .05 & set1_accuracy < set2_accuracy   ~ set2),
+      flag_adj = case_when(
+        is.na(p_value_adj)                                ~ "Zero variance for one/more sets",
+        p_value_adj > .05                                 ~ "Non-Significant difference",
+        p_value_adj < .05 & set1_accuracy > set2_accuracy ~ set1,
+        p_value_adj < .05 & set1_accuracy < set2_accuracy ~ set2))
 
   return(comps3)
 }
 
 winners <- find_winners(outputs_data = outputs, outputs_agg_data = outputs_aggregate, benchmark_data = benchmarks)
-
-# Retrieve mean accuracy and +/- 1 SD bars for each problem to plot
-
-
 
 #--------------------- Draw plots ------------------
 
@@ -196,26 +198,91 @@ winners <- find_winners(outputs_data = outputs, outputs_agg_data = outputs_aggre
 
 upper_tri <- data.frame(x = c(0, 0, 100), y = c(0, 100, 100))
 
+# Quickly arrange into "benchmark" and "features" columns for plotting then re-join
+
+set_data1 <- winner %>%
+  mutate(set_ind = ifelse(set1 %in% append(unique(outputs$method), "All features"),
+                          TRUE, FALSE)) %>%
+  filter(set_ind) %>%
+  dplyr::select(c(problem, set1, set1_accuracy, flag, flag_adj)) %>%
+  rename(method = set1,
+         set_accuracy = set1_accuracy)
+
+set_data2 <- winner %>%
+  mutate(set_ind = ifelse(set2 %in% append(unique(outputs$method), "All features"),
+                          TRUE, FALSE)) %>%
+  filter(set_ind) %>%
+  dplyr::select(c(problem, set2, set2_accuracy, flag, flag_adj)) %>%
+  rename(method = set2,
+         set_accuracy = set2_accuracy)
+
+bench_data1 <- winner %>%
+  mutate(set_ind = ifelse(set1 %in% unique(benchmarks$method),
+                          TRUE, FALSE)) %>%
+  filter(set_ind) %>%
+  dplyr::select(c(problem, set1, set1_accuracy, flag, flag_adj)) %>%
+  rename(method_bench = set1,
+         bench_accuracy = set1_accuracy)
+
+bench_data2 <- winner %>%
+  mutate(set_ind = ifelse(set2 %in% unique(benchmarks$method),
+                          TRUE, FALSE)) %>%
+  filter(set_ind) %>%
+  dplyr::select(c(problem, set2, set2_accuracy, flag, flag_adj)) %>%
+  rename(method_bench = set2,
+         bench_accuracy = set2_accuracy)
+
+set_final <- bind_rows(set_data1, set_data2)
+bench_final <- bind_rows(bench_data1, bench_data2)
+
+# Retrieve mean accuracy and +/- 1 SD bars for each problem to plot
+
+set_bars <- outputs_aggregate %>%
+  mutate(method = "All features") %>%
+  bind_rows(outputs) %>%
+  inner_join(set_final, by = c("problem" = "problem",
+                               "method" = "method")) %>%
+  group_by(problem, method, set_accuracy, flag, flag_adj) %>%
+  summarise(mean_x = mean(accuracy, na.rm = TRUE),
+            lower_x = mean(accuracy, na.rm = TRUE) - 1 * sd(accuracy, na.rm = TRUE),
+            upper_x = mean(accuracy, na.rm = TRUE) + 1 * sd(accuracy, na.rm = TRUE)) %>%
+  ungroup()
+
+bench_bars <- benchmarks %>%
+  inner_join(bench_final, by = c("problem" = "problem",
+                                 "method" = "method_bench")) %>%
+  group_by(problem, method, bench_accuracy, flag, flag_adj) %>%
+  summarise(mean_y = mean(accuracy, na.rm = TRUE),
+            lower_y = mean(accuracy, na.rm = TRUE) - 1 * sd(accuracy, na.rm = TRUE),
+            upper_y = mean(accuracy, na.rm = TRUE) + 1 * sd(accuracy, na.rm = TRUE)) %>%
+  ungroup() %>%
+  rename(method_bench = method)
+
+winner_final <- set_bars %>%
+  inner_join(bench_bars, by = c("problem" = "problem",
+                                "flag" = "flag", "flag_adj" = "flag_adj"))
+
 # Draw scatterplot
 
-p <- both %>%
-  ggplot(aes(x = worst_balanced_accuracy_mean, y = best_balanced_accuracy_mean)) +
+p <- winner_final %>%
+  mutate(across(c(mean_x, lower_x, upper_x,
+                  mean_y, lower_y, upper_y), ~ .x * 100)) %>%
+  ggplot(aes(x = mean_x, y = mean_y)) +
   geom_polygon(data = upper_tri, aes(x = x, y = y), fill = "steelblue2", alpha = 0.3) +
   geom_abline(intercept = 0, slope = 1, colour = "grey50", lty = "dashed") +
-  geom_errorbar(aes(ymin = lower_y, ymax = upper_y, colour = top_performer)) +
-  geom_errorbarh(aes(xmin = lower_x, xmax = upper_x, colour = top_performer)) +
-  geom_point(aes(colour = top_performer), size = 2) +
+  geom_errorbar(aes(ymin = lower_y, ymax = upper_y, colour = flag_adj)) +
+  geom_errorbarh(aes(xmin = lower_x, xmax = upper_x, colour = flag_adj)) +
+  geom_point(aes(colour = flag_adj), size = 2) +
   annotate("text", x = 80, y = 10, label = "Time-series features better") +
   annotate("text", x = 20, y = 90, label = "Leading benchmark better") +
   labs(title = "Comparison of feature sets versus benchmark algorithms across UCR/UEA repository univariate problems",
        subtitle = "Plots a subset of 50 problems where preliminary analysis showed mean and variance did not outperform chance",
        x = "Classification accuracy time-series features (%)",
        y = "Classification accuracy benchmark algorithm (%)",
-       colour = NULL,
-       shape = NULL) +
+       colour = NULL) +
   scale_x_continuous(labels = function(x)paste0(x, "%")) + 
   scale_y_continuous(labels = function(x)paste0(x, "%")) + 
-  scale_colour_distiller(palette = "Dark2") +
+  scale_colour_manual(values = RColorBrewer::brewer.pal(11, "Paired")) +
   theme_bw() +
   theme(legend.position = "bottom",
         legend.title = element_blank(),
