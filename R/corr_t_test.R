83bf62b84d31ad24c86f74a028cf4516c4c51a8a
Trent <trent.henderson1@outlook.com>
2022-11-21 15:26:27 +1100

    small comment

--- R/corr_t_test.R
+++ R/corr_t_test.R
@@ -17,9 +17,9 @@ corr_t_test <- function(x, y, n, n1, n2){
   t_stat <- d_bar / sqrt(sigma_2_mod) # Calculate t-statistic
   
   if(t_stat < 0){
-    p_val <- pt(t_stat, n - 1)
+    p_val <- pt(t_stat, n - 1) # p-value for left tail
   } else{
-    p_val <- pt(t_stat, n - 1, lower.tail = FALSE)
+    p_val <- pt(t_stat, n - 1, lower.tail = FALSE) # p-value for right tail
   }
   
   return(p_val)
