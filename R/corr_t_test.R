9254f3e464c06a13ce75567238d9d2731b010e88
Trent <trent.henderson1@outlook.com>
2022-11-21 15:23:32 +1100

    corrleated t stat

---
+++ R/corr_t_test.R
@@ -0,0 +1,26 @@
+#------------------------------------------
+# This script sets defines a function for 
+# calculating the correlated t-test statistic
+# presented in Nadeau & Bengio (2003)
+#------------------------------------------
+
+#------------------------------------------
+# Author: Trent Henderson, 21 November 2022
+#------------------------------------------
+
+corr_t_test <- function(x, y, n, n1, n2){
+  
+  d <- y - x # Calculate differences
+  d_bar <- mean(d, na.rm = TRUE) # Calculate mean of differences
+  sigma_2 <- var(d, na.rm = TRUE) # Calculate variance
+  sigma_2_mod <- sigma_2 * (1/n + n1/n2) # Calculate modified variance
+  t_stat <- d_bar / sqrt(sigma_2_mod) # Calculate t-statistic
+  
+  if(t_stat < 0){
+    p_val <- pt(t_stat, n - 1)
+  } else{
+    p_val <- pt(t_stat, n - 1, lower.tail = FALSE)
+  }
+  
+  return(p_val)
+}
