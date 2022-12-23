3cb97adc8e1cc11394bee41e1b8cf2245ede5757
Trent Henderson <trent.henderson1@outlook.com>
2022-12-23 19:37:38 +1100

    another case study

--- analysis/z-scored/case-studies.R
+++ analysis/z-scored/case-studies.R
@@ -18,7 +18,7 @@ outputs_z <- outputs_z %>%
     method == "tsfel" ~ "TSFEL",
     method == "kats"  ~ "Kats",
     TRUE              ~ method)) %>%
-  filter(problem %in% c("EthanolLevel", "CricketY", "ChlorineConcentration")) # Case study problems of interest
+  filter(problem %in% c("EthanolLevel", "CricketY", "ChlorineConcentration", "ArrowHead")) # Case study problems of interest
 
 # Load raw time series
 
@@ -33,6 +33,9 @@ CricketY <- TimeSeriesData %>%
 ChlorineConcentration <- TimeSeriesData %>%
   filter(problem == "ChlorineConcentration")
 
+ArrowHead <- TimeSeriesData %>%
+  filter(problem == "ArrowHead")
+
 rm(TimeSeriesData)
 
 # Load feature calculations
@@ -43,6 +46,8 @@ load("data/feature-calcs/z-scored/CricketY.Rda")
 CricketY_feats <- outs_z
 load("data/feature-calcs/z-scored/ChlorineConcentration.Rda")
 ChlorineConcentration_feats <- outs_z
+load("data/feature-calcs/z-scored/ArrowHead.Rda")
+ArrowHead_feats <- outs_z
 rm(outs_z)
 
 # Calculate overall mean performance for each problem and set
@@ -159,19 +164,19 @@ plot_samples(data = EthanolLevel, n = 2, seed = 123)
 # Identify top features
 
 EthanolLevel_top <- compute_top_features2(EthanolLevel_feats, 
-                                   id_var = "id", 
-                                   group_var = "group",
-                                   num_features = 40, 
-                                   method = "z-score",
-                                   test_method = "svmLinear",
-                                   use_balanced_accuracy = TRUE,
-                                   use_k_fold = TRUE,
-                                   num_folds = 10,
-                                   use_empirical_null =  TRUE,
-                                   null_testing_method = "ModelFreeShuffles",
-                                   p_value_method = "gaussian",
-                                   num_permutations = 1000,
-                                   seed = 123)
+                                          id_var = "id", 
+                                          group_var = "group",
+                                          num_features = 40, 
+                                          method = "z-score",
+                                          test_method = "svmLinear",
+                                          use_balanced_accuracy = TRUE,
+                                          use_k_fold = TRUE,
+                                          num_folds = 10,
+                                          use_empirical_null =  TRUE,
+                                          null_testing_method = "ModelFreeShuffles",
+                                          p_value_method = "gaussian",
+                                          num_permutations = 1000,
+                                          seed = 123)
 
 save(EthanolLevel_top, file = "data/EthanolLevel_top.Rda")
 
@@ -190,19 +195,19 @@ plot_samples(data = CricketY, n = 3, seed = 123)
 # Identify top features
 
 CricketY_top <- compute_top_features2(CricketY_feats, 
-                                       id_var = "id", 
-                                       group_var = "group",
-                                       num_features = 40,
-                                       method = "z-score",
-                                       test_method = "svmLinear",
-                                       use_balanced_accuracy = TRUE,
-                                       use_k_fold = TRUE,
-                                       num_folds = 10,
-                                       use_empirical_null =  TRUE,
-                                       null_testing_method = "ModelFreeShuffles",
-                                       p_value_method = "gaussian",
-                                       num_permutations = 1000,
-                                       seed = 123)
+                                      id_var = "id", 
+                                      group_var = "group",
+                                      num_features = 40,
+                                      method = "z-score",
+                                      test_method = "svmLinear",
+                                      use_balanced_accuracy = TRUE,
+                                      use_k_fold = TRUE,
+                                      num_folds = 10,
+                                      use_empirical_null =  TRUE,
+                                      null_testing_method = "ModelFreeShuffles",
+                                      p_value_method = "gaussian",
+                                      num_permutations = 1000,
+                                      seed = 123)
 
 save(CricketY_top, file = "data/CricketY_top.Rda")
 
@@ -221,19 +226,19 @@ plot_samples(data = ChlorineConcentration, n = 2, seed = 123)
 # Identify top features
 
 ChlorineConcentration_top <- compute_top_features2(ChlorineConcentration_feats, 
-                                     id_var = "id", 
-                                     group_var = "group",
-                                     num_features = 40, 
-                                     method = "z-score",
-                                     test_method = "svmLinear",
-                                     use_balanced_accuracy = TRUE,
-                                     use_k_fold = TRUE,
-                                     num_folds = 10,
-                                     use_empirical_null =  TRUE,
-                                     null_testing_method = "ModelFreeShuffles",
-                                     p_value_method = "gaussian",
-                                     num_permutations = 1000,
-                                     seed = 123)
+                                                   id_var = "id", 
+                                                   group_var = "group",
+                                                   num_features = 40, 
+                                                   method = "z-score",
+                                                   test_method = "svmLinear",
+                                                   use_balanced_accuracy = TRUE,
+                                                   use_k_fold = TRUE,
+                                                   num_folds = 10,
+                                                   use_empirical_null =  TRUE,
+                                                   null_testing_method = "ModelFreeShuffles",
+                                                   p_value_method = "gaussian",
+                                                   num_permutations = 1000,
+                                                   seed = 123)
 
 save(ChlorineConcentration_top, file = "data/ChlorineConcentration_top.Rda")
 
@@ -242,3 +247,34 @@ save(ChlorineConcentration_top, file = "data/ChlorineConcentration_top.Rda")
 ChlorineConcentration_plot <- plot_all_ts(data = ChlorineConcentration)
 print(ChlorineConcentration_plot)
 ggsave("output/ChlorineConcentration_sample.pdf", plot = ChlorineConcentration_plot, units = "in", height = 12, width = 10)
+
+#------------------ Case study IV: ArrowHead -----------------
+
+# Draw plot
+
+plot_samples(data = ArrowHead, n = 2, seed = 123)
+
+# Identify top features
+
+ArrowHead_top <- compute_top_features2(ArrowHead_feats, 
+                                       id_var = "id", 
+                                       group_var = "group",
+                                       num_features = 40, 
+                                       method = "z-score",
+                                       test_method = "svmLinear",
+                                       use_balanced_accuracy = TRUE,
+                                       use_k_fold = TRUE,
+                                       num_folds = 10,
+                                       use_empirical_null =  TRUE,
+                                       null_testing_method = "ModelFreeShuffles",
+                                       p_value_method = "gaussian",
+                                       num_permutations = 1000,
+                                       seed = 123)
+
+save(ArrowHead_top, file = "data/ArrowHead_top.Rda")
+
+# Draw plots like in the catch22 paper
+
+ArrowHead_plot <- plot_all_ts(data = ArrowHead)
+print(ArrowHead_plot)
+ggsave("output/ArrowHead_sample.pdf", plot = ArrowHead_plot, units = "in", height = 12, width = 10)
