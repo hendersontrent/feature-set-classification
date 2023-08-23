4becac1579e9deb73970eaef2e7abbe526222d7a
Trent <trent.henderson1@outlook.com>
2023-08-23 23:01:42 +1000

    top features

--- analysis/z-scored/analyse_top-features.R
+++ analysis/z-scored/analyse_top-features.R
@@ -21,4 +21,14 @@ rm(data_files)
 
 #--------------- Calculate top features --------------
 
+top_features <- vector(mode = "list", length = length(individual_feats))
 
+for(i in 1:length(individual_feats)){
+  message(paste0("Doing ", match(i, 1:length(top_features)), "/", length(top_features)))
+  comps <- compare_features(individual_feats[[i]], by_set = FALSE, hypothesis = "null")
+  comps$problem <- unique(individual_feats[[i]]$ClassificationResults$problem)
+  top_features[[i]] <- comps
+}
+
+top_features <- do.call("rbind", top_features)
+save(top_features, file = "data/top_features.Rda")
